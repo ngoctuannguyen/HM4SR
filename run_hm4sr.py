e3089c40fee06db2e19220fec456fb099fbc647d
Nguyễn Tuấn Ngọc <59074969+ngoctuannguyen@users.noreply.github.com>
2025-03-29 19:35:40 +0000

    feat: ml-1m dataset

--- run_hm4sr.py
+++ run_hm4sr.py
@@ -82,6 +82,6 @@ def print_result(test_result, logger, k=4):
 
 
 if __name__ == '__main__':
-    dataset = 'Games'
+    dataset = 'ml-1m'
     run_recbole(model='HM4SR', dataset=dataset,
                 config_file_list=['./config/data.yaml', f'./config/{dataset}.yaml'])
